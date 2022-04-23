d74dedfe43f165ec6d3639f064ed0f1c11c702be
Asim Mehar <asimmeharproffessional@gmail.com>
2022-04-23 03:00:05 -0700

    volunteer app services

--- app/src/main/java/com/example/vis/Student/StudentTimer.java
+++ app/src/main/java/com/example/vis/Student/StudentTimer.java
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.vis.R;
+import com.example.vis.databinding.ActivityStudentTimerBinding;
+import com.example.vis.databinding.ActivityWelcomeStudentBinding;
 
 public class StudentTimer extends AppCompatActivity {
 
+    public ActivityStudentTimerBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student_timer);
+        binding = ActivityStudentTimerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
     }
 }

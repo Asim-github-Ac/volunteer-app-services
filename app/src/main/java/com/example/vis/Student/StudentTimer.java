93ed8e523c8667405e5037b83c7b750eb1ca58ac
Asim Mehar <asimmeharproffessional@gmail.com>
2022-04-23 09:45:50 -0700

    volunteer app services

--- app/src/main/java/com/example/vis/Student/StudentTimer.java
+++ app/src/main/java/com/example/vis/Student/StudentTimer.java
@@ -1,8 +1,21 @@
 package com.example.vis.Student;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Toast;
 
@@ -10,15 +23,22 @@ import com.example.vis.R;
 import com.example.vis.databinding.ActivityStudentTimerBinding;
 import com.example.vis.databinding.ActivityWelcomeStudentBinding;
 
-public class StudentTimer extends AppCompatActivity {
-
+public class StudentTimer extends AppCompatActivity implements LocationListener {
+    private static final int REQUEST_LOCATION = 1;
+    LocationManager locationManager;
+    String latitude, longitude;
     public ActivityStudentTimerBinding binding;
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityStudentTimerBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        progressDialog=new ProgressDialog(this);
+        progressDialog.setTitle("Location Verification");
+        progressDialog.setMessage("Waiting.....");
+        progressDialog.setCancelable(false);
 
         binding.confirmnow.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +47,7 @@ public class StudentTimer extends AppCompatActivity {
                     Toast.makeText(StudentTimer.this, "Something Went Wrong", Toast.LENGTH_SHORT).show();
                 }else {
                     ConfirmOrder();
+                    progressDialog.show();
                 }
             }
         });
@@ -34,5 +55,75 @@ public class StudentTimer extends AppCompatActivity {
 
     private void ConfirmOrder() {
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            OnGPS();
+        } else {
+            getLocation();
+        }
     }
+    private void OnGPS() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Enable GPS").setCancelable(false).setPositiveButton("Yes", new  DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                StudentTimer.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                StudentTimer.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        } else {
+          getLocations();
+        }
+    }
+    public  void getLocations() {
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, this);
+        }
+        catch(SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+      String lat = String.valueOf(location.getLatitude());
+      String newlat=lat.substring(0,6);
+      if (newlat.equals("33.546")){
+          progressDialog.dismiss();
+
+      }else {
+          Toast.makeText(this, "Location not found", Toast.LENGTH_SHORT).show();
+          progressDialog.dismiss();
+      }
+     //   Toast.makeText(this, ""+location.getLatitude(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(StudentTimer.this, "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
 }

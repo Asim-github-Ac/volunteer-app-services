46498e4e2e92b1cfa4f9f9669e59de3eee0a3103
Asim Mehar <asimmeharproffessional@gmail.com>
2022-04-23 03:05:27 -0700

    volunteer app services

--- app/src/main/java/com/example/vis/Student/StudentTimer.java
+++ app/src/main/java/com/example/vis/Student/StudentTimer.java
@@ -3,6 +3,8 @@ package com.example.vis.Student;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.vis.R;
 import com.example.vis.databinding.ActivityStudentTimerBinding;
@@ -17,6 +19,20 @@ public class StudentTimer extends AppCompatActivity {
         binding = ActivityStudentTimerBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
+        binding.confirmnow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (binding.startdate.getText().toString().isEmpty() && binding.starttime.getText().toString().isEmpty() && binding.endtime.getText().toString().isEmpty()) {
+                    Toast.makeText(StudentTimer.this, "Something Went Wrong", Toast.LENGTH_SHORT).show();
+                }else {
+                    ConfirmOrder();
+                }
+            }
+        });
+    }
+
+    private void ConfirmOrder() {
 
     }
 }

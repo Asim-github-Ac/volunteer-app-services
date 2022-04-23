b61f3fa4478a29fb0809d7fed29ed424b817f233
Asim Mehar <asimmeharproffessional@gmail.com>
2022-04-23 02:51:36 -0700

    volunteer app services

---
+++ app/src/main/java/com/example/vis/Student/StudentTimer.java
@@ -0,0 +1,16 @@
+package com.example.vis.Student;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vis.R;
+
+public class StudentTimer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_timer);
+    }
+}
--- app/src/main/java/com/example/vis/WelcomeStudentActivity.java
+++ app/src/main/java/com/example/vis/WelcomeStudentActivity.java
@@ -1,6 +1,7 @@
 package com.example.vis;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,19 +12,30 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.example.vis.SharedPrefrence.PrefManager;
+import com.example.vis.Student.StudentTimer;
 import com.example.vis.Student.SubmitReport;
 import com.example.vis.databinding.ActivityWelcomeStudentBinding;
+import com.google.android.material.navigation.NavigationView;
 
 
 public class WelcomeStudentActivity extends AppCompatActivity {
     private ActivityWelcomeStudentBinding mActivityBinding;
     PrefManager prefManager;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mActivityBinding = ActivityWelcomeStudentBinding.inflate(getLayoutInflater());
         setContentView(mActivityBinding.getRoot());
         prefManager=new PrefManager(this);
+        setSupportActionBar(mActivityBinding.topLogo);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, mActivityBinding.drawerstd, R.string.nav_open, R.string.nav_close);
+        mActivityBinding.drawerstd.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mActivityBinding.husnatkhalid.setText(prefManager.getUserName());
         mActivityBinding.studentBtnProject.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,7 +64,33 @@ public class WelcomeStudentActivity extends AppCompatActivity {
             startActivity(new Intent(getApplicationContext(), SubmitReport.class));
         }
     });
+    mActivityBinding.navviewstd.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.addloc:
+                    Intent intent=new Intent(getApplicationContext(), StudentTimer.class);
+                    startActivity(intent);
+                    break;
+            }
+            return true;
+        }
+    });
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            switch (item.getItemId()){
+                case R.id.addloc:
+                    Intent intent=new Intent(getApplicationContext(),WelcomeStudentActivity.class);
+                    startActivity(intent);
+                    break;
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
